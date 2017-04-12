52ebe6747a2b02b37986d901a549334d632772db
Gordon Elliott <gordon.e.elliott@gmail.com>
2017-04-12 14:41:59 +0100

    Expose Funds through GraphQL

--- src/glod/api/fund_node.py
+++ src/glod/api/fund_node.py
@@ -3,12 +3,16 @@ __copyright__ = 'Copyright(c) Gordon Elliott 2017'
 """ 
 """
 
-from glod.api.fund_leaf import FundLeaf
+from graphene.relay import Node
+
+from glod.api.fund_leaf import FundLeaf, fund_fields
 from glod.db.fund import FundQuery
 
 
 class FundNode(FundLeaf):
-    pass
+    class Meta:
+        interfaces = (Node,)
+        local_fields = fund_fields
 
 
 def resolve_funds(self, args, context, info):
