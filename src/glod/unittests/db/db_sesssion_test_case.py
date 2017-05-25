364bb0b3212ea2b5d002c96c863789a543f592bb
Gordon Elliott <gordon.e.elliott@gmail.com>
2017-05-25 17:29:36 +0100

    Clean up test databases

--- src/glod/unittests/db/db_sesssion_test_case.py
+++ src/glod/unittests/db/db_sesssion_test_case.py
@@ -8,16 +8,15 @@ from uuid import uuid1
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from a_tuin.db.metadata import metadata, truncate_all
 from glod.configuration import configuration
 
 
 TEST_DB_NAME = 'test_{}'.format(uuid1().hex)
+DB_NAME_FILE = '/tmp/glod_unittest_db_name.txt'
 LOG = logging.getLogger(__name__)
-
-# TODO drop test schema if all tests complete successfully
 
 
 class DBSessionTestCase(TestCase):
@@ -34,6 +33,19 @@ class DBSessionTestCase(TestCase):
             create_database(cls.engine.url)
 
             metadata.create_all(cls.engine)
+
+            # Make sure old test dbs are cleaned up
+            with open(DB_NAME_FILE, 'r') as db_name_file:
+
+                for old_db_name in db_name_file:
+                    LOG.info('Dropping old test database {}'.format(old_db_name))
+                    drop_database(
+                        configuration.db.connection_template.format(old_db_name)
+                    )
+
+            with open(DB_NAME_FILE, 'w') as db_name_file:
+
+                db_name_file.write('{}\n'.format(TEST_DB_NAME))
 
     @classmethod
     def tearDownClass(cls):
