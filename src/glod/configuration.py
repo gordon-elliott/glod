02552c645807716d40d54cacc84e5e541ec86d9d
jde-rabbit <jde.cherryview@gmail.com>
2024-09-05 10:31:27 +0100

    use importlib instead of pkg_resources

--- src/glod/configuration.py
+++ src/glod/configuration.py
@@ -5,11 +5,11 @@ __copyright__ = 'Copyright(c) Gordon Elliott 2017'
 
 import logging.config
 import yaml
-import pkg_resources
+import importlib_resources
 
 from dotmap import DotMap
 
-config_path = pkg_resources.resource_filename(__name__, 'config/dev.yaml')
+config_path = importlib_resources.files(__name__) / 'config/dev.yaml'
 
 with open(config_path, 'r') as ymlfile:
     configuration = DotMap(yaml.full_load(ymlfile))
