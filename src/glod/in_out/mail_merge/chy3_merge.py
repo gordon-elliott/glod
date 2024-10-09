bc4215c66489207d271b0402328caac10d4a07e5
Gordon Elliott <gordon-elliott@users.noreply.github.com>
2024-10-09 18:38:07 +0100

    Dynamically pick up cover letter fields (#48)

--- src/glod/in_out/mail_merge/chy3_merge.py
+++ src/glod/in_out/mail_merge/chy3_merge.py
@@ -13,13 +13,14 @@ from a_tuin.in_out.google_drive import (
     upload_to_gdrive,
     PDF_MIME_TYPE,
 )
-from a_tuin.in_out.google_docs import merge_letter
+from a_tuin.in_out.google_docs import merge_letter, template_doc_properties
 from a_tuin.in_out.pdf_merge import fill_form, concatenate
 
 from glod.in_out.mail_merge.letter_merge import read_from_gsheet
 
 LOG = logging.getLogger(__name__)
 
+REF = "ref"
 DONOR = "Name of Donor"
 PPS = "PPSN"
 POSTAL_ADDRESS = "Address incl. Eircode"
@@ -27,6 +28,8 @@ PHONE = "Phone No"
 EMAIL = "Email Address"
 CHARITY_NAME = "Name of eligible charity or other approved body"
 VALID_FROM = "0"
+
+FORM_FIELDS = (DONOR, PPS, POSTAL_ADDRESS, PHONE, EMAIL)
 
 
 def _merge_letters(
@@ -37,41 +40,45 @@ def _merge_letters(
     template_file_id,
     template_filename,
     valid_from_tax_year,
+    cover_merge_fields,
     targets,
 ):
-    for given_name, donor, pps, postal_address, phone, email, household_id, ref in targets:
-        LOG.info(f"Merging letter for {given_name}, household id: {household_id}")
-        cover_letter_filename = f"cover.{household_id}.pdf"
+    for target in targets:
+        form_dict = dict(zip(FORM_FIELDS, target[:len(FORM_FIELDS)]))
+        cover_dict = dict(zip(cover_merge_fields, target[-1 * len(cover_merge_fields):]))
+
+        ref = cover_dict.get(REF)
+        cover_letter_filename = f"cover.{ref}.pdf"
         cover_letter_path = os.path.join(temp_dir, cover_letter_filename)
-        replacements = dict(titles=given_name, ref=ref, Label=postal_address)
-        # todo dynamically find and use fields from worksheet
-        with merge_letter(
-            gdrive, gdocs, template_file_id, replacements
-        ) as merged_file_id:
+
+        LOG.info(f"Merging letter for {cover_dict} to {cover_letter_path}.")
+
+        with merge_letter(gdrive, gdocs, template_file_id, cover_dict) as merged_file_id:
             download_as_pdf(gdrive, merged_file_id, cover_letter_path)
 
-        chy3_filename = f"chy3.{household_id}.pdf"
+        chy3_filename = f"chy3.{ref}.pdf"
         chy3_file_path = os.path.join(temp_dir, chy3_filename)
         merge_data = {
-            DONOR: donor,
-            PPS: pps,
-            POSTAL_ADDRESS: postal_address,
-            PHONE: phone,
-            EMAIL: email,
             CHARITY_NAME: configuration.charity.name,
             VALID_FROM: str(valid_from_tax_year),
         }
+        merge_data.update(form_dict)
         fill_form(template_filename, chy3_file_path, merge_data)
 
-        output_file = os.path.join(temp_dir, f"combined.{household_id}.pdf")
+        output_file = os.path.join(temp_dir, f"combined.{ref}.pdf")
         concatenate([cover_letter_path, chy3_file_path], output_file)
 
         yield output_file
 
 
 def merge_chy3_letters(
-    configuration, input_workbook_file_id, sheet_name, merge_fields, template_letter_file_id, empty_certificate_form
+    configuration, input_workbook_file_id, sheet_name, form_merge_fields, template_letter_file_id, empty_certificate_form
 ):
+    if len(form_merge_fields) != len(FORM_FIELDS):
+        raise ValueError(
+            f"There are too many or too few form merge fields. {len(form_merge_fields)} found; {len(FORM_FIELDS)} expected."
+        )
+
     current_year = date.today().year
     valid_from_tax_year = current_year - 2001
 
@@ -80,6 +87,12 @@ def merge_chy3_letters(
 
     working_folder = "."
     full_merge_pdf_filename = f"chy3_letters_from_{sheet_name}.pdf"
+    _, cover_merge_fields = template_doc_properties(gdocs, template_letter_file_id)
+
+    if REF not in cover_merge_fields:
+        raise ValueError(f"Template cover letter must include a field titled '{REF}'.")
+
+    merge_fields = form_merge_fields + cover_merge_fields
 
     targets = read_from_gsheet(configuration, input_workbook_file_id, sheet_name, merge_fields)
     with TemporaryDirectory(dir=working_folder, prefix=f"chy3_merge_") as temp_dir:
@@ -92,6 +105,7 @@ def merge_chy3_letters(
                 template_letter_file_id,
                 empty_certificate_form,
                 valid_from_tax_year,
+                cover_merge_fields,
                 targets,
             )
         )
